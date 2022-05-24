fc7930bd07b5f14ee3b2aa91f1df8e57816d70ff
Lukas Goßmann <lg4161666@arcor.de>
2022-05-24 21:14:02 +0200

    Made output of SpxReader a bit prettier

--- SpxReader/Program.cs
+++ SpxReader/Program.cs
@@ -6,13 +6,36 @@ namespace SpxReader
     {
         public static void Main(string[] args)
         {
-            var filename = "";
-            var spxFile = SpxFile.Open(filename);
+            var spxFile = SpxFile.Open(args[0]);
+
+            Console.WriteLine($"RemainingFileLength1: {spxFile.RemainingFileLength1}");
+            Console.WriteLine($"ProducedWithLength: {spxFile.ProducedWithLength}");
+            Console.WriteLine($"ProducedWithString: {spxFile.ProducedWithString}");
+            Console.WriteLine($"RemainingFileLength2: {spxFile.RemainingFileLength2}");
+            Console.WriteLine($"SizeOfVariableLengthData: {spxFile.SizeOfVariableLengthData}");
+            Console.WriteLine($"SpxStruct.StitchCountLimit: {spxFile.SpxStruct.StitchCountLimit.Int}");
+            Console.WriteLine($"SpxStruct.StitchOffset: {spxFile.SpxStruct.StitchOffset.Int}");
+            Console.WriteLine($"SpxStruct.StartTaperSide: {spxFile.SpxStruct.StartTaperSide}");
+            Console.WriteLine($"SpxStruct.EndTaperSide: {spxFile.SpxStruct.EndTaperSide}");
+            Console.WriteLine($"SpxStruct.StartTaperAngle: {spxFile.SpxStruct.StartTaperAngle}");
+            Console.WriteLine($"SpxStruct.EndTaperAngle: {spxFile.SpxStruct.EndTaperAngle}");
+            Console.WriteLine($"SpxStruct.StringTension: {spxFile.SpxStruct.StringTension}");
+            Console.WriteLine($"SpxStruct.StitchHeight: {spxFile.SpxStruct.StitchHeight.Int}");
+            Console.WriteLine($"SpxStruct.StitchWidth: {spxFile.SpxStruct.StitchWidth.Int}");
+            Console.WriteLine($"SpxStruct.ResolutionX: {spxFile.SpxStruct.ResolutionX.Int}");
+            Console.WriteLine($"SpxStruct.ResolutionY: {spxFile.SpxStruct.ResolutionY.Int}");
+            Console.WriteLine($"SpxStruct.StartPositionX: {spxFile.SpxStruct.StartPositionX.Int}");
+            Console.WriteLine($"SpxStruct.StitchArrayLength: {spxFile.SpxStruct.StitchArrayLength.Int}");
+
+            Console.WriteLine();
+
             var stitchPositions = spxFile.DecodeStitchPositions();
 
+            Console.WriteLine($"     X     |     Y");
+            Console.WriteLine($"-----------|-----------");
             foreach (var position in stitchPositions)
             {
-                Console.WriteLine($"{position.X} {position.Y}");
+                Console.WriteLine($"{position.X.ToString().PadLeft(10)} | {position.Y.ToString().PadLeft(10)}");
             }
         }
     }
